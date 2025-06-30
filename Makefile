2d44b0a584babdb4f5c9e7afae823e09ef17d198
Sang Cao <hoaisang0806@gmail.com>
2025-06-30 20:41:10 +0700

    Install packages and create get / route

--- Makefile
+++ Makefile
@@ -3,8 +3,13 @@
 dev: serve
 	@go run cmd/server/main.go
 
-serve: 
+serve: go_serve tailwind_serve
+
+go_serve:
 	@go mod tidy
 
-build:
-	@go build -o build/server cmd/server/main.go
+tailwind_serve:
+	@npx @tailwindcss/cli -i static/css/tailwind.css -o static/css/output.css -m
+
+watch:
+	@npx @tailwindcss/cli -i static/css/tailwind.css -o static/css/output.css -wm & air
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"html/template"
+	"io"
+)
+
+type TemplateRenderer struct {
+	templates *template.Template
+}
+
+func (t *TemplateRenderer) Render(w io.Writer, name string, data any, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
+func newTemplateRenderer() *TemplateRenderer {
+	return &TemplateRenderer{templates: template.Must(template.ParseGlob("template/*.templ"))}
+}
+
+type App struct {
+	Title string
+	Name  string
+}
 
 func main() {
-	fmt.Println("The app is not yet available.")
+	e := echo.New()
+	e.Use(middleware.Logger())
+	e.Renderer = newTemplateRenderer()
+
+	app := &App{
+		Title: "Conway's The Game of Life",
+		Name:  "World",
+	}
+
+	e.GET("/", func(c echo.Context) error {
+		return c.Render(200, "index", app)
+	})
+
+	e.Static("/static", "static")
+
+	e.Logger.Fatal(e.Start(":8386"))
 }
